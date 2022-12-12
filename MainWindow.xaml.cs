ba1a6d0a6f6a84c2bf422ec48fa13627faf41560
исп-31 <-31@LAB17-07>
2022-12-12 14:16:31 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows;
+
+namespace Pr13v7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mItEx_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вариант 7. " +
+               "Дана матрица размера M * N. Найти номера строки и столбца для " +
+               "элемента матрицы, наиболее близкого к среднему значению всех ее элементов.",
+               "Задание",
+               MessageBoxButton.OK,
+               MessageBoxImage.Asterisk);
+        }
+
+        private void mItDeveloper_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("10",
+               "Разработчик",
+               MessageBoxButton.OK,
+               MessageBoxImage.Asterisk);
+        }
+
+        private void mItExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        Matrix<int> matrix = new Matrix<int>(0, 0);
+
+        private void btnFill_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MatrixManipulations.Create(matrix, 
+                Convert.ToInt32(txtboxRows.Text), 
+                Convert.ToInt32(txtboxColumns.Text), 
+                Convert.ToInt32(txtboxRangeMin.Text), 
+                Convert.ToInt32(txtboxRangeMax.Text));
+
+                dgMatrix1.ItemsSource = matrix.ToDataTable().DefaultView;
+
+                txtboxTableSize.Text = $"{Convert.ToInt32(txtboxRows.Text)}:{Convert.ToInt32(txtboxColumns.Text)}";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введены неверные данные",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }           
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @".\matrix" + matrix.Extension;
+            matrix.Save(path);
+            MessageBox.Show("Отлично!",
+                "Сохранение",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @".\matrix" + matrix.Extension;
+            matrix.Open(path);
+            dgMatrix1.ItemsSource = matrix.ToDataTable().DefaultView;
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            txtboxResultRow.Text = MatrixManipulations.CalculateRow(matrix).ToString();
+            txtboxResultColumn.Text = MatrixManipulations.CalculateColumn(matrix).ToString();
+        }
+
+        private void btnClean_Click(object sender, RoutedEventArgs e)
+        {
+            txtboxRows.Clear();
+            txtboxColumns.Clear();
+            txtboxRangeMin.Clear();
+            txtboxRangeMax.Clear();
+            txtboxResultRow.Clear();
+            txtboxResultColumn.Clear();
+            txtboxCellNumber.Clear();
+            txtboxTableSize.Clear();
+        }
+
+        private void dgMatrix1_CellEditEnding(object sender, System.Windows.Controls.DataGridCellEditEndingEventArgs e)
+        {
+            txtboxCellNumber.Text = $"{e.Row.GetIndex()}:{e.Column.DisplayIndex}";
+            //Int32 selectedColumnCount = dgMatrix1.Columns;
+        }
+
+        private void txtboxRows_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            txtboxResultRow.Clear();
+            txtboxResultColumn.Clear();
+            txtboxTableSize.Clear();
+            txtboxCellNumber.Clear();
+        }
+
+    }
+}
---
+++ MatrixManipulations.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Pr13v7
+{
+    internal static class MatrixManipulations
+    {
+        public static void Create(this Matrix<int> numbers, int row, int column, int minValue, int maxValue)
+        {
+            int[,] matrix = new int[row, column];
+            Random rnd = new Random();
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    matrix[i, j] = rnd.Next(minValue, maxValue);
+                }
+            }
+            numbers.Add(matrix);
+        }
+
+        public static int CalculateColumn(this Matrix<int> matrix)
+        {
+            int sum = 0,
+                average = 0,               
+                localRow = 0,
+                localColumn = 0,
+                i;
+            for (i = 0; i < matrix.Rows; i++)
+            {
+                for (int j = 0; j < matrix.Columns; j++)
+                {
+                    sum += matrix[i, j]; //1. Нахождение суммы
+                }
+            }
+
+            average = sum / (matrix.Rows * matrix.Columns); //2. Нахождение среднего значения
+
+            int minItem = matrix[0,0];
+            for (i = 0; i < matrix.Rows; i++)
+            {
+                for (int j = 0; j < matrix.Columns; j++)
+                {
+                    if (Math.Abs(average - matrix[i,j]) < minItem) //3. Поиск минимальной разницы между средней величиной и исходной
+                    {
+                        minItem = matrix[i, j];
+                        localRow = i+1;
+                        localColumn = j+1;
+                    }
+                }
+
+            }
+
+            //int[,]ints = new int[localRow,localColumn];
+
+            return localColumn;
+        }
+
+        public static int CalculateRow(this Matrix<int> matrix)
+        {
+            int sum = 0,
+                average = 0,
+                localRow = 0,
+                localColumn = 0,
+                i;
+            for (i = 0; i < matrix.Rows; i++)
+            {
+                for (int j = 0; j < matrix.Columns; j++)
+                {
+                    sum += matrix[i, j]; //1. Нахождение суммы
+                }
+            }
+
+            average = sum / (matrix.Rows * matrix.Columns); //2. Нахождение среднего значения
+
+            int minItem = matrix[0, 0];
+            for (i = 0; i < matrix.Rows; i++)
+            {
+                for (int j = 0; j < matrix.Columns; j++)
+                {
+                    if (Math.Abs(average - matrix[i, j]) < minItem) //3. Поиск минимальной разницы между средней величиной и исходной
+                    {
+                        minItem = matrix[i, j];
+                        localRow = i + 1;
+                        localColumn = j + 1;
+                    }
+                }
+
+            }
+
+            //int[,]ints = new int[localRow,localColumn];
+
+            return localRow;
+        }
+    }
+}
+
+                        
+           
+         
